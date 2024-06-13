6fc19198bd3f793e7e4c0533689148965285ff81
vitosandrin <sandrinvito10@gmail.com>
2024-06-13 00:44:31 -0300

    config
    Update to .NET 8.0 and Docker enhancements
    
    - Updated `.dockerignore` to optimize Docker build context by excluding unnecessary files and directories such as `.git`, `.vscode`, `node_modules`, etc., while including specific `.git` files.
    - Upgraded `Catalog.Application`, `Catalog.CrossCutting`, `Catalog.Infrastructure`, and `Catalog.Domain` projects to target `.NET 8.0`, enabling `ImplicitUsings` and `Nullable` reference types for improved code quality.
    - Added `DependencyInjection.cs` in `Catalog.CrossCutting` for configuring services and MediatR, adhering to clean architecture.
    - Updated `EcommerceServer.sln` to enhance solution structure and project management within Visual Studio.
    - Configured `Catalog.API` project for `.NET 8.0`, nullable reference types, and Docker development, including a new `Dockerfile` for containerization.
    - Introduced a basic "Hello World!" API endpoint in `Catalog.API` for foundational API development.
    - Revised `launchSettings.json` and added `appsettings.json` & `appsettings.Development.json` for flexible environment configuration and logging.

---
+++ src/Catalog.CrossCutting/DependencyInjection.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+using MediatR;
+using Microsoft.Data.SqlClient;
+
+using Catalog.Domain.Abstractions;
+using Catalog.Infrastructure.Context;
+using Catalog.Infrastructure.Repositories;
+
+namespace CqrsDemo.CrossCutting.AppDependencies;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var dbConnectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
+        services.AddSingleton<IDbConnection>(provider =>
+        {
+            var connection = new SqlConnection(dbConnectionString);
+            connection.Open();
+            return connection;
+        });
+
+        services.AddScoped<IMemberRepository, MemberRepository>();
+        services.AddScoped<IUnityOfWork, UnityOfWork>();
+        services.AddScoped<IMemberDapperRepository, MemberDapperRepository>();
+
+        var handlersAssembly = Assembly.Load("CqrsDemo.Application");
+
+        services.AddMediatR(cfg =>
+        {
+            //register all handlers from the assembly
+            cfg.RegisterServicesFromAssemblies(handlersAssembly);
+            //register all validators from the assembly
+            cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+        });
+
+        services.AddValidatorsFromAssembly(handlersAssembly);
+
+        return services;
+    }
+}
