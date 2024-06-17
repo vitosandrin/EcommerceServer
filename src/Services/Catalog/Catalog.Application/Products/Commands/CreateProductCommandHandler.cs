2fba1dfa8adbdd5142d1c83ed866b098854258d6
vitosandrin <sandrinvito10@gmail.com>
2024-06-17 01:07:27 -0300

    wip: CreateProductCommandHandler

---
+++ src/Services/Catalog/Catalog.Application/Products/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Contracts.DataTransferObjects;
+using Contracts.Services.Catalog;
+using MediatR;
+
+namespace Catalog.Application.Products.Commands;
+
+internal class CreateProductCommandHandler : IRequestHandler<Command.CreateProduct, Command.Result.CreateProduct>
+{
+    public async Task<Command.Result.CreateProduct> Handle(Command.CreateProduct request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
