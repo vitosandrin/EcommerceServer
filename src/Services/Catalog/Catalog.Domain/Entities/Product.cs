30edd1512eee33187064b09675f2d82b82bc5aa8
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-18 14:41:09 -0300

    fix: remove attribute from object

--- src/Services/Catalog/Catalog.Domain/Entities/Product.cs
+++ src/Services/Catalog/Catalog.Domain/Entities/Product.cs
@@ -6,6 +6,5 @@ public class Product
     public string Name { get; set; } = default!;
     public List<string> Category { get; set; } = new();
     public string Description { get; set; } = default!;
-    public string ImageFile { get; set; } = default!;
     public decimal Price { get; set; }
 }
