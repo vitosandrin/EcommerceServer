3510d44b77b595ae92e1d3d7c3299b8b9ba2c0f6
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-18 14:39:45 -0300

    fix: change id to guid

--- src/Contracts/Services/Catalog/Command.cs
+++ src/Contracts/Services/Catalog/Command.cs
@@ -4,13 +4,11 @@ namespace Contracts.Services.Catalog;
 public class Command
 {
     public record CreateProduct(string Name, string Description, List<string> Category, long Price) : ICommand<Result.CreateProduct>;
-    public record UpdateProduct(int Id, string Name, List<string> Category, string Description, long Price);
-    public record DeleteProductById(int Id);
-    public record GetProductById(int Id);
-    public record GetProducts;
+    public record UpdateProduct(Guid Id, string Name, List<string> Category, string Description, long Price);
+    public record DeleteProductById(Guid Id);
 
     public class Result
     {
-        public record CreateProduct(int Id);
+        public record CreateProduct(Guid Id);
     }
 }
