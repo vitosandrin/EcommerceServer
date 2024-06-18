f9d68f96bf70272898493db6412bd168fd55a8dd
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-18 11:04:47 -0300

    feat: create Http contracts for request and response of CreateProduct

---
+++ src/Contracts/Services/Catalog/Http.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts.Services.Catalog;
+
+public class Http
+{
+    public class Request
+    {
+        public record CreateProduct(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    }
+    public class Response
+    {
+        public record CreateProduct(Guid Id);
+    }
+}
