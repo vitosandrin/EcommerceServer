1cde10915ca82cb6d0b0c0280e3d2672e85f39ce
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-18 11:02:38 -0300

    chore: add global using import statements

---
+++ src/Services/Catalog/Catalog.CrossCutting/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using System.Reflection;
+global using Carter;
+global using Marten;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using FluentValidation;
+global using Contracts.Behaviors;
+global using Contracts.Abstractions;
