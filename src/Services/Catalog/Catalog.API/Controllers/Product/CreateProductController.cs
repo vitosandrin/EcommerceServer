0de16aee0b1d38fdb6d98e20aaf5f797bfdfa66f
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-18 11:05:51 -0300

    feat: CreateProductController

---
+++ src/Services/Catalog/Catalog.API/Controllers/Product/CreateProductController.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Services.Catalog;
+
+namespace Catalog.API.Controllers.Product;
+public class CreateProductController : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (Http.Request.CreateProduct request, ISender sender) =>
+        {
+            var command = request.Adapt<Command.CreateProduct>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<Http.Response.CreateProduct>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        }).WithName("CreateProduct")
+        .Produces<Http.Response.CreateProduct>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
+}
