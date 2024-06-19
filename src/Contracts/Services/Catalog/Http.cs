e111d2567af6ec77c6b11388150e885e74e391b6
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-19 10:59:04 -0300

    feat: GetAllProductsController contracts

--- src/Contracts/Services/Catalog/Http.cs
+++ src/Contracts/Services/Catalog/Http.cs
@@ -1,14 +1,17 @@
-﻿namespace Contracts.Services.Catalog;
+﻿using Contracts.DataTransferObjects;
+
+namespace Contracts.Services.Catalog;
 
 public class Http
 {
     public class Request
     {
         public record CreateProduct(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
-
+        public record GetAllProducts();
     }
     public class Response
     {
         public record CreateProduct(Guid Id);
+        public record GetAllProducts(IEnumerable<Product> Products);
     }
 }
