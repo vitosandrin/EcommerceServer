b399f77549dc0ef0cebc7bdd8891c9c5b744d9b5
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-19 10:59:34 -0300

    feat: GetAllProductsController

---
+++ src/Services/Catalog/Catalog.API/Controllers/Product/GetAllProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Contracts.Services.Catalog;
+namespace Catalog.API.Controllers.Product;
+
+public class GetAllProductsController : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async ([AsParameters] Http.Request.GetAllProducts request, ISender sender) =>
+        {
+            var result = await sender.Send(new Query.GetAllProducts());
+
+            var response = result.Adapt<Query.Result.GetAllProducts>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetAllProducts")
+        .Produces<Http.Response.GetAllProducts>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("GetAllProduct")
+        .WithDescription("GetAllProducts");
+
+    }
+}
