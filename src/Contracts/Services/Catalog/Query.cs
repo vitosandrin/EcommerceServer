dddbe4ca5e256a0a5c791ca97df4de10262b9e50
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-19 10:58:46 -0300

    feat: GetAllProductsQueryHandler Contracts

---
+++ src/Contracts/Services/Catalog/Query.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.DataTransferObjects;
+
+namespace Contracts.Services.Catalog;
+
+public class Query
+{
+    public record GetAllProducts() : IQuery<Result.GetAllProducts>;
+
+    public class Result
+    {
+        public record GetAllProducts(IEnumerable<Product> Products);
+    }
+}
