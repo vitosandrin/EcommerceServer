5d6c5861bac511dc95a59b3082de0337c1ec9c99
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-19 11:04:41 -0300

    fix: remove unused using statements

--- src/Services/Catalog/Catalog.CrossCutting/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.CrossCutting/GlobalUsing.cs
@@ -4,4 +4,3 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using FluentValidation;
 global using Contracts.Behaviors;
-global using Contracts.Abstractions;
