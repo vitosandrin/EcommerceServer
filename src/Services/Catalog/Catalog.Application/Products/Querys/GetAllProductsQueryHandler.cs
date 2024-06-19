d837c6475a5b078f0ffecab42d1066a0e8d5bec8
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-19 10:58:24 -0300

    feat: GetAllProductsQuery Handler

---
+++ src/Services/Catalog/Catalog.Application/Products/Querys/GetAllProductsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.DataTransferObjects;
+using Contracts.Services.Catalog;
+using Marten;
+
+namespace Catalog.Application.Products.Querys;
+
+internal class GetAllProductsQueryHandler(IDocumentSession session) : IQueryHandler<Query.GetAllProducts, Query.Result.GetAllProducts>
+{
+    public async Task<Query.Result.GetAllProducts> Handle(Query.GetAllProducts query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new Query.Result.GetAllProducts(products);
+    }
+}
