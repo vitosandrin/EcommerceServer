af5095811039556bdda7df9b5e6c46538e40856a
vitosandrin <sandrinvito10@gmail.com>
2024-06-20 00:27:15 -0300

    feat: Http Contracts for GetProductByIdCOntroller

--- src/Contracts/Services/Catalog/Http.cs
+++ src/Contracts/Services/Catalog/Http.cs
@@ -8,10 +8,12 @@ public class Http
     {
         public record CreateProduct(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
         public record GetAllProducts();
+        public record GetProductById(Guid Id);
     }
     public class Response
     {
         public record CreateProduct(Guid Id);
         public record GetAllProducts(IEnumerable<Product> Products);
+        public record GetProductById(Product Product);
     }
 }
