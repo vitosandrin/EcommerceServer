32e7ed299577e9c4e5084a4caf4047331f702512
vitosandrin <sandrinvito10@gmail.com>
2024-06-20 00:27:32 -0300

    feat: Query Contracts for GetProductByIdQueryHandler

--- src/Contracts/Services/Catalog/Query.cs
+++ src/Contracts/Services/Catalog/Query.cs
@@ -6,9 +6,11 @@ namespace Contracts.Services.Catalog;
 public class Query
 {
     public record GetAllProducts() : IQuery<Result.GetAllProducts>;
+    public record GetProductById(Guid Id) : IQuery<Result.GetProductById>;
 
     public class Result
     {
         public record GetAllProducts(IEnumerable<Product> Products);
+        public record GetProductById(Product Product);
     }
 }
