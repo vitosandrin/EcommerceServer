58073385cb47b62b9d5aeecae28e9b77ab7abe74
vitosandrin <sandrinvito10@gmail.com>
2024-06-20 00:45:03 -0300

    feat: create query record for GetProductByCategoryQueryHandler

---
+++ src/Services/Catalog/Catalog.Application/Products/Querys/GetProductByCategoryHandler.cs
@@ -0,0 +1,19 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.Services.Catalog;
+using Contracts.DataTransferObjects;
+
+using Marten;
+
+namespace Catalog.Application.Products.Querys;
+
+internal class GetProductByCategoryHandler(IDocumentSession session) : IQueryHandler<Query.GetProductsByCategory, Query.Result.GetProductsByCategory>
+{
+    public async Task<Query.Result.GetProductsByCategory> Handle(Query.GetProductsByCategory query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>()
+                   .Where(p => p.Category.Contains(query.Category))
+                   .ToListAsync(cancellationToken);
+
+        return new Query.Result.GetProductsByCategory(products);
+    }
+}
