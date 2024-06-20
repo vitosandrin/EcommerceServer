e33b64b4b5eb6d9ee26fa805f9972009f384d492
vitosandrin <sandrinvito10@gmail.com>
2024-06-20 00:27:52 -0300

    feat: GetProductById Handler

---
+++ src/Services/Catalog/Catalog.Application/Products/Querys/GetProductByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.Exceptions;
+using Contracts.Services.Catalog;
+using Contracts.DataTransferObjects;
+
+using Marten;
+
+namespace Catalog.Application.Products.Querys;
+
+internal class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<Query.GetProductById, Query.Result.GetProductById>
+{
+    public async Task<Query.Result.GetProductById> Handle(Query.GetProductById query, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+            throw new NotFoundException($"Product with id {query.Id} not found.");
+
+        return new Query.Result.GetProductById(product);
+    }
+}
