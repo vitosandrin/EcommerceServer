46b714a00d7854accfc930374f359fa646934c3e
vitosandrin <sandrinvito10@gmail.com>
2024-06-20 00:44:45 -0300

    feat: create query record for GetProductByQueryHandler

--- src/Contracts/Services/Catalog/Query.cs
+++ src/Contracts/Services/Catalog/Query.cs
@@ -7,10 +7,13 @@ public class Query
 {
     public record GetAllProducts() : IQuery<Result.GetAllProducts>;
     public record GetProductById(Guid Id) : IQuery<Result.GetProductById>;
+    public record GetProductsByCategory(string Category) : IQuery<Result.GetProductsByCategory>;
 
     public class Result
     {
         public record GetAllProducts(IEnumerable<Product> Products);
         public record GetProductById(Product Product);
+        public record GetProductsByCategory(IEnumerable<Product> Products);
+
     }
 }
