340e948e3332cba682502bc53227617f0d13517d
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-21 11:51:19 -0300

    hotfix: product validations

---
+++ src/Contracts/DataTransferObjects/Product/CreateProductCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Services.Catalog;
+using FluentValidation;
+
+namespace Contracts.DataTransferObjects;
+
+public class CreateProductCommandValidator : AbstractValidator<Command.CreateProduct>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
