78961669cdb8d00c4fa58508e6e180bd51723431
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:16:01 -0300

    feat: ShoppingCartItem and ShoppingCart DTOs

---
+++ src/Contracts/DataTransferObjects/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.DataTransferObjects;
+public record ShoppingCartItem(int Quantity, string Color, long Price, Guid ProductId, string ProductName);
+public record ShoppingCart(string UserName, List<ShoppingCartItem> Items)
+{
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart() : this(string.Empty, new List<ShoppingCartItem>())
+    {
+    }
+}
