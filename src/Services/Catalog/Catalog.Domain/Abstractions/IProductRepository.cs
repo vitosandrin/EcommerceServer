b22c0c465379327ad6e7be6aa66f05f50bfe508f
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-24 15:50:55 -0300

    feat: product repository

---
+++ src/Services/Catalog/Catalog.Domain/Abstractions/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Abstractions;
+using Contracts.DataTransferObjects;
+
+namespace Catalog.Domain.Abstractions;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProductsPaginated(int PageNumber, int PageSize, CancellationToken cancellationToken);
+    Task<Product> GetProductById(Guid Id, CancellationToken cancellationToken);
+    Task<IEnumerable<Product>> GetProductByCategory(string Category, CancellationToken cancellationToken);
+    Task<Guid> CreateProduct(Product Product, CancellationToken cancellationToken);
+    Task<bool> UpdateProduct(Product Product, CancellationToken cancellationToken);
+    Task<bool> DeleteProduct(Guid Id, CancellationToken cancellationToken);
+}
---
+++ src/Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using Catalog.Domain.Abstractions;
+using Catalog.Infrastructure.Exceptions;
+using Contracts.DataTransferObjects;
+using Contracts.Exceptions;
+using Marten;
+using Marten.Pagination;
+using System.Threading;
+
+namespace Catalog.Infrastructure.Repositories;
+
+public class ProductRepository(IDocumentSession session) : IProductRepository
+{
+    private readonly IDocumentSession _session = session;
+    public async Task<IEnumerable<Product>> GetAllProductsPaginated(int PageNumber, int PageSize, CancellationToken cancellationToken)
+    {
+        return await _session.Query<Product>()
+                           .ToPagedListAsync(PageNumber, PageSize, cancellationToken);
+    }
+
+    public async Task<Product> GetProductById(Guid Id, CancellationToken cancellationToken)
+    {
+        var product = await _session.LoadAsync<Product>(Id, cancellationToken);
+
+        if (product is null)
+            throw new ProductNotFoundException(Id);
+
+        return product;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByCategory(string Category, CancellationToken cancellationToken)
+    {
+        return await _session.Query<Product>()
+                   .Where(p => p.Category.Contains(Category))
+                   .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Guid> CreateProduct(Product Product, CancellationToken cancellationToken)
+    {
+        _session.Store(Product);
+        await _session.SaveChangesAsync(cancellationToken);
+        return Product.Id;
+    }
+
+    public async Task<bool> UpdateProduct(Product Product, CancellationToken cancellationToken)
+    {
+        var product = await _session.LoadAsync<Product>(Product.Id, cancellationToken);
+
+        if (product is null)
+            throw new ProductNotFoundException(Product.Id);
+
+        product.Name = Product.Name;
+        product.Category = Product.Category;
+        product.Description = Product.Description;
+        product.Price = Product.Price;
+
+        _session.Update(product);
+        await _session.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+
+    public async Task<bool> DeleteProduct(Guid Id, CancellationToken cancellationToken)
+    {
+        var product = await _session.LoadAsync<Product>(Id, cancellationToken);
+
+        if (product is null)
+            throw new ProductNotFoundException(Id);
+
+        _session.Delete<Product>(Id);
+
+        await _session.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
