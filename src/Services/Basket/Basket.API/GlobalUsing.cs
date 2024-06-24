ae0e71a6edf413efe916342a21d94ab6958cb057
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:56:57 -0300

    chore: add dependencies

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,4 +1,7 @@
 ﻿global using Basket.CrossCutting;
 global using Carter;
+global using Contracts.Services.Basket;
 global using HealthChecks.UI.Client;
+global using Mapster;
+global using MediatR;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
