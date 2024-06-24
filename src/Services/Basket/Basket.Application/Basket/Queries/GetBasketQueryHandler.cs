8d3d6b79eaad6d26e9d1fa955c28f98f2bb559f6
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:38:52 -0300

    wip: GetBasketQueryHandler

---
+++ src/Services/Basket/Basket.Application/Basket/Queries/GetBasketQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.DataTransferObjects;
+using Contracts.Services.Basket;
+
+namespace Basket.Application.Basket.Queries;
+
+internal class GetBasketQueryHandler : IQueryHandler<Query.GetBasket, Query.Result.GetBasket>
+{
+    public async Task<Query.Result.GetBasket> Handle(Query.GetBasket query, CancellationToken cancellationToken)
+    {
+        return new Query.Result.GetBasket(new ShoppingCart());
+    }
+}
