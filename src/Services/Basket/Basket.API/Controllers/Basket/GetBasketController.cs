c267ac7e8a741d798c480b3c15319b158fa78a0a
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:38:12 -0300

    wip: GetBasketController

---
+++ src/Services/Basket/Basket.API/Controllers/Basket/GetBasketController.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Controllers.Basket;
+
+public class GetBasketController : ICarterModule
+{
+    public void AddRoutes(IRouteBuilder routeBuilder)
+    {
+        routeBuilder.Get("basket", async () =>
+        {
+            var username = routeData.Get<string>("username");
+            var response = await req.SendRequestAsync<Http.Response.GetBasket>(Http.Request.GetBasket(username));
+            return res.WriteAsJsonAsync(response.Cart);
+        });
+    }
+}
