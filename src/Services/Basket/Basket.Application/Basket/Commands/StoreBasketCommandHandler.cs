7822cc4f30f2692c659aa6df66ee494f713189a5
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:57:09 -0300

    wip: StoreBasketCommandHandler

---
+++ src/Services/Basket/Basket.Application/Basket/Commands/StoreBasketCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.Services.Basket;
+
+namespace Basket.Application.Basket.Commands;
+
+internal class StoreBasketCommandHandler : ICommandHandler<Command.StoreBasket, Command.Result.StoreBasket>
+{
+    public async Task<Command.Result.StoreBasket> Handle(Command.StoreBasket command, CancellationToken cancellationToken)
+    {
+        return new Command.Result.StoreBasket(true);
+    }
+}
