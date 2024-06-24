403469d1e274fadf3bef0ebd5cfb788953f23ecd
vitosandrin <sandrinvito10@gmail.com>
2024-06-23 23:38:01 -0300

    feat: GEtBasket query and http

--- src/Contracts/Services/Basket/Http.cs
+++ src/Contracts/Services/Basket/Http.cs
@@ -1,8 +1,17 @@
-﻿namespace Contracts.Services.Basket;
+﻿using Contracts.DataTransferObjects;
+
+namespace Contracts.Services.Basket;
 
 public class Http
 {
-    public class Request { }
+    public class Request
+    {
+        public record GetBasket(string Username);
+    }
 
-    public class Response { }
+    public class Response
+    {
+        public record GetBasket(ShoppingCart Cart);
+
+    }
 }
--- src/Contracts/Services/Basket/Query.cs
+++ src/Contracts/Services/Basket/Query.cs
@@ -1,9 +1,12 @@
-﻿namespace Contracts.Services.Basket;
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.DataTransferObjects;
+namespace Contracts.Services.Basket;
 
 public class Query
 {
+    public record GetBasket(string UserName) : IQuery<Result.GetBasket>;
     public class Result
     {
-
+        public record GetBasket(ShoppingCart Cart);
     }
 }
