bae04de1e8c030048df47ad2267b8cff39136b2d
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-25 16:03:11 -0300

    feat: interface discountService

---
+++ src/Services/Discount/Discount.gRPC/Abstractions/IDiscountService.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Core;
+
+namespace Discount.gRPC.Abstractions;
+public interface IDiscountService
+{
+    Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context);
+    Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context);
+    Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context);
+    Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context);
+}
