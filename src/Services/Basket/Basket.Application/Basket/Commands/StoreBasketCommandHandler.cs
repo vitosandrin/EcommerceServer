3e577ca87b36f381a0546f10fcfffd47c8014057
vitosandrin <sandrinvito10@gmail.com>
2024-06-24 23:35:40 -0300

    feat: store basket

--- src/Services/Basket/Basket.Application/Basket/Commands/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.Application/Basket/Commands/StoreBasketCommandHandler.cs
@@ -1,12 +1,16 @@
-﻿using Contracts.Abstractions.CQRS;
+﻿using Basket.Domain.Abstractions;
+using Contracts.Abstractions.CQRS;
 using Contracts.Services.Basket;
 
 namespace Basket.Application.Basket.Commands;
 
-internal class StoreBasketCommandHandler : ICommandHandler<Command.StoreBasket, Command.Result.StoreBasket>
+internal class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<Command.StoreBasket, Command.Result.StoreBasket>
 {
+    private readonly IBasketRepository _basketRepository = repository;
     public async Task<Command.Result.StoreBasket> Handle(Command.StoreBasket command, CancellationToken cancellationToken)
     {
-        return new Command.Result.StoreBasket("ok");
+        await _basketRepository.StoreBasket(command.Cart, cancellationToken);
+
+        return new Command.Result.StoreBasket(command.Cart.UserName);
     }
 }
