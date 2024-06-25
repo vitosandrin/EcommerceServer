d2b4ecaf2c56ebe3bb106bfd3efdd9d7806cefc2
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-25 16:07:38 -0300

    fix: identation

--- src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -13,6 +13,6 @@ public class DiscountContext(DbContextOptions<DiscountContext> options) : DbCont
         modelBuilder.Entity<Coupon>().HasData(
             new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
             new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
-            );
+        );
     }
 }
