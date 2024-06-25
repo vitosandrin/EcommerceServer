1f192ae90dd47dd91af701ddcd16a9b1370c9b78
vitosandrin <sandrinvito10@gmail.com>
2024-06-24 23:37:40 -0300

    feat: delete basket command handler

--- src/Services/Basket/Basket.Application/Basket/Commands/DeleteBasketCommandHandler.cs
+++ src/Services/Basket/Basket.Application/Basket/Commands/DeleteBasketCommandHandler.cs
@@ -1,12 +1,16 @@
-﻿using Contracts.Abstractions.CQRS;
+﻿using Basket.Domain.Abstractions;
+using Contracts.Abstractions.CQRS;
 using Contracts.Services.Basket;
 
 namespace Basket.Application.Basket.Commands;
 
-internal class DeleteBasketCommandHandler : ICommandHandler<Command.DeleteBasket, Command.Result.DeleteBasket>
+internal class DeleteBasketCommandHandler(IBasketRepository repository) : ICommandHandler<Command.DeleteBasket, Command.Result.DeleteBasket>
 {
+    private readonly IBasketRepository _basketRepository = repository;
     public async Task<Command.Result.DeleteBasket> Handle(Command.DeleteBasket command, CancellationToken cancellationToken)
     {
+        await _basketRepository.DeleteBasket(command.UserName, cancellationToken);
+
         return new Command.Result.DeleteBasket(true);
     }
 }
