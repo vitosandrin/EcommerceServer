c754a59a6e08b11f3507f0d44030abed30ce45dc
vitosandrin <sandrinvito10@gmail.com>
2024-06-24 23:37:48 -0300

    feat: get basket query handler

--- src/Services/Basket/Basket.Application/Basket/Queries/GetBasketQueryHandler.cs
+++ src/Services/Basket/Basket.Application/Basket/Queries/GetBasketQueryHandler.cs
@@ -1,13 +1,16 @@
-﻿using Contracts.Abstractions.CQRS;
-using Contracts.DataTransferObjects;
+﻿using Basket.Domain.Abstractions;
+using Contracts.Abstractions.CQRS;
 using Contracts.Services.Basket;
 
 namespace Basket.Application.Basket.Queries;
 
-internal class GetBasketQueryHandler : IQueryHandler<Query.GetBasket, Query.Result.GetBasket>
+internal class GetBasketQueryHandler(IBasketRepository repository) : IQueryHandler<Query.GetBasket, Query.Result.GetBasket>
 {
+    private readonly IBasketRepository _basketRepository = repository;
     public async Task<Query.Result.GetBasket> Handle(Query.GetBasket query, CancellationToken cancellationToken)
     {
-        return new Query.Result.GetBasket(new ShoppingCart());
+        var basket = await _basketRepository.GetBasket(query.UserName, cancellationToken);
+
+        return new Query.Result.GetBasket(basket);
     }
 }
