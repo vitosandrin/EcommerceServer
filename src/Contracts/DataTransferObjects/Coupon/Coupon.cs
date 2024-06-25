71e0f695254dcea39e1cb4e73b1ec2703d32161c
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-25 16:01:37 -0300

    feat: create Coupon DTO for Discount service

---
+++ src/Contracts/DataTransferObjects/Coupon/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.DataTransferObjects;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public long Amount { get; set; }
+}
