803e30168900b00e37b76e5d1091c19bfec7c130
Vito Sandrin <sandrinvito10@gmail.com>
2024-06-26 16:02:36 -0300

    fix: global using Discount.gRPC in basket.crosscutting

--- src/Services/Basket/Basket.CrossCutting/DependencyInjection.cs
+++ src/Services/Basket/Basket.CrossCutting/DependencyInjection.cs
@@ -1,6 +1,4 @@
-﻿using Discount.gRPC;
-
-namespace Basket.CrossCutting;
+﻿namespace Basket.CrossCutting;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
--- src/Services/Basket/Basket.CrossCutting/GlobalUsing.cs
+++ src/Services/Basket/Basket.CrossCutting/GlobalUsing.cs
@@ -8,3 +8,4 @@ global using Marten;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Reflection;
+global using Discount.gRPC;
