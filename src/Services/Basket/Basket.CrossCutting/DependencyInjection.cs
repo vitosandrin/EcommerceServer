93e7d61d2c4d640decfd21cd8d72b1b71d6b7676
vitosandrin <sandrinvito10@gmail.com>
2024-06-25 23:46:39 -0300

    fix

--- src/Services/Basket/Basket.CrossCutting/DependencyInjection.cs
+++ src/Services/Basket/Basket.CrossCutting/DependencyInjection.cs
@@ -1,13 +1,10 @@
 ﻿namespace Basket.CrossCutting;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         var handlersAssembly = Assembly.Load("Basket.Application");
         var validatorsAssembly = Assembly.Load("Contracts");
-
-        services.AddScoped<IBasketRepository, BasketRepository>();
-        services.Decorate<IBasketRepository, CachedBasketRepository>();
 
         services.AddMediatR(config =>
         {
@@ -17,6 +14,19 @@ public static class DependencyInjection
         });
         services.AddValidatorsFromAssembly(validatorsAssembly);
         services.AddExceptionHandler<CustomExceptionHandler>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBasketRepository, BasketRepository>();
+        services.Decorate<IBasketRepository, CachedBasketRepository>();
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
         services.AddMarten(opts =>
         {
             opts.Connection(configuration.GetConnectionString("Database")!);
