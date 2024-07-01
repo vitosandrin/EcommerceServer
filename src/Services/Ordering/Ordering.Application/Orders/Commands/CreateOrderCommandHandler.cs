4bb13fb0efe18fc91eebb280f90aa803d5d1b1ca
vitosandrin <sandrinvito10@gmail.com>
2024-06-30 23:29:13 -0300

    wip: createorderhandler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrderCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Abstractions.CQRS;
+using Contracts.Services.Ordering;
+using Ordering.Infrastructure.Abstractions;
+
+namespace Ordering.Application.Orders.Commands;
+public class CreateOrderHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<Command.CreateOrder, Command.Result.CreateOrder>
+{
+    private readonly IApplicationDbContext _dbContext = dbContext;
+    public Task<Command.Result.CreateOrder> Handle(Command.CreateOrder request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
